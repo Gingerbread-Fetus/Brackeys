fd7bb3cb8c247156e2c40f01d195cb969e5e1b58
Hayden A. M <haydenamatthews@gmail.com>
2023-02-13 23:24:18 -0700

    Got basic movement in and started playing around with animations

---
+++ BrackeysJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public InputAction playerControls;
+
+    Rigidbody2D rb;
+    Vector2 moveDirection;
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Awake()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = playerControls.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
